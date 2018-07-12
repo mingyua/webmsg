bcab1a0e2f9a86b84cab45d03de7213018c84819
mingyu <285412937@qq.com>
2018-07-12 17:39:18 +0800

    add admin moban

---
+++ public/manage/index.js
@@ -0,0 +1,16 @@
+/** layuiAdmin.pro-v1.0.0 LPPL License By http://www.layui.com/admin/ */ ;
+layui.extend({
+	//admin: "admin"
+}).define('element', function(e) {
+	var 
+		n = layui.element,
+		//i = layui.admin,
+		//t = i.tabsPage,
+		//l = layui.view,
+		s = "#LAY_app_body",
+		u = "layadmin-layout-tabs",
+		d = layui.$,
+		c = d(window);
+	layui.link(base + "admin.css");
+	//alert(base);
+});
