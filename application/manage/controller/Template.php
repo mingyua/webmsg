8649d04d3695cb8baf0839c087b64f4c6fdb17a9
mingyu <285412937@qq.com>
2018-08-31 18:00:31 +0800

    add codeedit

--- application/manage/controller/Template.php
+++ application/manage/controller/Template.php
@@ -18,6 +18,21 @@ class Template extends Auth
     	$this->assign('templist',json_encode($templist));
        return view();
     }
+    public function edit()
+    {
+    	$id=input('tempid');
+    	$template=db('template')->where('id',$id)->find();
+		$file_path = "./template/home/".$template['template'];
+		if(file_exists($file_path)){
+			$fp = fopen($file_path,"r");
+			$str = fread($fp,filesize($file_path));//指定读取大小，这里把整个文件内容读取出来
+			$this->assign('content',$str);
+		}else{
+			$this->assign('content','文件不存在');
+		}   
+		$this->assign('template',$template);	
+       return view();
+    }
 
 
 }
