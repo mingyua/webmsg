469988adbe2fea1d689ada26c0141c6abb11a493
mingyu <285412937@qq.com>
2018-09-03 14:49:38 +0800

    add codeedit CodeMirror

--- application/manage/controller/Template.php
+++ application/manage/controller/Template.php
@@ -20,18 +20,33 @@ class Template extends Auth
     }
     public function edit()
     {
-    	$id=input('tempid');
-    	$template=db('template')->where('id',$id)->find();
-		$file_path = "./template/home/".$template['template'];
-		if(file_exists($file_path)){
-			$fp = fopen($file_path,"r");
-			$str = fread($fp,filesize($file_path));//指定读取大小，这里把整个文件内容读取出来
-			$this->assign('content',$str);
-		}else{
-			$this->assign('content','文件不存在');
-		}   
-		$this->assign('template',$template);	
-       return view();
+     	if($this->request->post()){
+     		$post=$this->request->post();
+     		if(isset($post['file'])){
+				$myfile = fopen("./template/home/".$post['file'], "w") or die("Unable to open file!");
+				$txt = $post['desc'];
+				fwrite($myfile, $txt);
+				fclose($myfile);     			
+     			$back=['msg'=>'操作成功！','status'=>1,'icon'=>1,'url'=>''];	    	
+     		}else{
+     			$back=['msg'=>'操作1！','status'=>1,'icon'=>1,'url'=>''];	    	
+     		}
+     		return $back;die();
+     	}else{
+	    	$id=input('tempid');
+	    	$template=db('template')->where('id',$id)->find();
+			$file_path = "./template/home/".$template['template'];
+			if(file_exists($file_path)){
+				$fp = fopen($file_path,"r");
+				$str = fread($fp,filesize($file_path));//指定读取大小，这里把整个文件内容读取出来
+				$this->assign('content',$str);
+			}else{
+				$this->assign('content','文件不存在');
+			}   
+			$this->assign('template',$template);
+
+	       return view();
+       }	
     }
 
 
