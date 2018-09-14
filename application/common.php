9d68eb0c03ebd2a8161141c9a0b31431c716cdf2
mingyu <285412937@qq.com>
2018-09-14 09:25:37 +0800

    add extend wxpay

--- application/common.php
+++ application/common.php
@@ -25,9 +25,6 @@ function getconfigfield($field){
  * $arr 数据
  * 
  */
-
-
-
 function fieldhtml($arr){
 	$html='';
 	foreach($arr as $k=>$v){
@@ -309,4 +306,44 @@ function dyconfig($getname,$pos){
 
 function addtext($text,$addword){
 	return $text.$addword;
+}
+
+//加密函数
+function lock_url($txt,$key='mingyu')
+{
+  $txt = $txt.$key;
+  $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=+";
+  $nh = rand(0,64);
+  $ch = $chars[$nh];
+  $mdKey = md5($key.$ch);
+  $mdKey = substr($mdKey,$nh%8, $nh%8+7);
+  $txt = base64_encode($txt);
+  $tmp = '';
+  $i=0;$j=0;$k = 0;
+  for ($i=0; $i<strlen($txt); $i++) {
+    $k = $k == strlen($mdKey) ? 0 : $k;
+    $j = ($nh+strpos($chars,$txt[$i])+ord($mdKey[$k++]))%64;
+    $tmp .= $chars[$j];
+  }
+  return urlencode(base64_encode($ch.$tmp));
+}
+//解密函数
+function unlock_url($txt,$key='mingyu')
+{
+  $txt = base64_decode(urldecode($txt));
+  $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=+";
+  $ch = $txt[0];
+  $nh = strpos($chars,$ch);
+  $mdKey = md5($key.$ch);
+  $mdKey = substr($mdKey,$nh%8, $nh%8+7);
+  $txt = substr($txt,1);
+  $tmp = '';
+  $i=0;$j=0; $k = 0;
+  for ($i=0; $i<strlen($txt); $i++) {
+    $k = $k == strlen($mdKey) ? 0 : $k;
+    $j = strpos($chars,$txt[$i])-$nh - ord($mdKey[$k++]);
+    while ($j<0) $j+=64;
+    $tmp .= $chars[$j];
+  }
+  return trim(base64_decode($tmp),$key);
 }
